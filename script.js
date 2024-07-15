16a89db972c4065255d31008a656fa9f6fad7243
WalterGomes13 <167585120+WalterGomes13@users.noreply.github.com>
2024-07-15 18:13:01 -0300

    Atividades-aula-11

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+Atividade 01
+const array = [50, 45, 67, 89, 10, 5];
+
+const newArray = array.map(elemento=>elemento*2);
+
+console.log(newArray)*/
+
+/*Atividade 02
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+const pares = numbers.filter(numero=>numero%2===0)
+
+console.log(pares)*/
+
+//Atividade 03
+/*const numbers = [1, 2, 3, 4, 5, 6]
+
+const soma = numbers.reduce((acum,num)=>acum+num,0);
+console.log(soma)*/
+
+//Atividade 04
+/*const lista = [1,2,3,4]
+const novaLista = lista.map(e=>e**2)
+console.log(novaLista)*/
+
+//Atividade  06
+const palavras = ['maça','banana','uva','laranja','pera','melancia']
+
+const novaWord = palavras.filter(p=>p.length>5);
+console.log(novaWord)
